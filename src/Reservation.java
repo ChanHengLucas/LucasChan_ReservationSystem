3295ef0a0b4073274980b803aca73090b2222405
ChanHengLucas <lucaschanheng12345@gmail.com>
2024-01-31 09:07:14 -0500

    Initial commit

---
+++ src/Reservation.java
@@ -0,0 +1,32 @@
+public class Reservation {
+    private String name; //not implemented yet lol
+    private int nPeople;
+    private int timeMade;
+    private int timeFor;
+    private boolean waitlisted; //not implemented yet
+
+    public Reservation() {
+        nPeople = (int)(Math.random() * 10) + 1;
+        timeFor = (int) (Math.random() * 960) + 360;
+        timeMade = (int) (Math.random() * 1440);
+    }
+
+    public void printInfo(){
+        if (timeFor % 60 < 10 && timeMade % 60 < 10){
+            System.out.println("Reservation for " + nPeople + " at " + (int)(timeFor/60) + ":0" + (int)(timeFor%60) + " made at " + (int)(timeMade/60) + ":0" + (int)(timeMade%60) );
+        } else if (timeFor % 60 < 10){
+            System.out.println("Reservation for " + nPeople + " at " + (int)(timeFor/60) + ":0" + (int)(timeFor%60) + " made at " + (int)(timeMade/60) + ":" + (int)(timeMade%60) );
+        } else if (timeMade % 60 < 10){
+            System.out.println("Reservation for " + nPeople + " at " + (int)(timeFor/60) + ":" + (int)(timeFor%60) + " made at " + (int)(timeMade/60) + ":0" + (int)(timeMade%60) );
+        } else {
+            System.out.println("Reservation for " + nPeople + " at " + (int) (timeFor / 60) + ":" + (int) (timeFor % 60) + " made at " + (int) (timeMade / 60) + ":" + (int) (timeMade % 60));
+        }
+    }
+
+    public int getNPeople(){
+        return nPeople;
+    }
+    public int getTimeFor() { return timeFor; }
+    public int getTimeMade() { return timeMade; }
+
+}
---
+++ src/Restaurant.java
@@ -0,0 +1,210 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Restaurant {
+
+    // textfield for adding new reservations manually
+    private ArrayList<Reservation> guestList = new ArrayList<>(25);
+
+    // 1) How do I make it so guest list contains 3 integers (nPeople, timeFor, timeMade)
+    // 1.5) What is the type "Reservation" and what types of variables is guest list supposed to contain
+    // 2) How do I make it sort the first or second or third integer of the variables stored in guest list
+
+    public Restaurant(){
+        addReservation(); // only ran once
+        displayReservations();
+    }
+
+    public void displayReservations(){
+
+        int nOfReservations = 25; // number of res
+
+        for (int i = 0; i < nOfReservations; i++){
+//            guestList.add(nPeopleList.get(i), timeForList.get(i), timeMadeList.get(i));
+
+        }
+
+        // guestList size
+
+        for (int i = 0; i < guestList.size(); i++){
+            guestList.get(i).printInfo();
+        }
+    } // might not need anymore after GUI exists
+
+
+    public void addReservation(){
+        for (int i = 0; i < 25; i++) {
+            guestList.add(new Reservation());
+        }
+        sortByTimeFor(); // default = sort by time for
+//        System.out.println(guestList.get(0).getNPeople());
+    }
+
+    public void sortByNPeople(){
+        Reservation temp;
+        boolean stop = true;
+        for (int i = 0; i < guestList.size(); i++){
+            for (int j = 0; j < guestList.size(); j++) {
+                while (j != 0 && guestList.get(j).getNPeople() < guestList.get(j-1).getNPeople()){
+                    stop = false;
+                    temp = guestList.get(j-1);
+                    guestList.set(j-1, guestList.get(j));
+                    guestList.set(j, temp);
+                }
+            }
+            if (stop == true){
+                break;
+            }
+//            for (int k = 0; k < nPeopleList.size(); k++){
+//                System.out.print(nPeopleList.get(k) + " ");
+//            }
+//            System.out.println();
+        }
+    }
+
+    public void sortByTimeFor(){
+
+            Reservation temp;
+            boolean stop = true;
+            for (int i = 0; i < guestList.size(); i++){
+                for (int j = 0; j < guestList.size(); j++) {
+                    while (j != 0 && guestList.get(j).getTimeFor() < guestList.get(j-1).getTimeFor()){
+                        stop = false;
+                        temp = guestList.get(j-1);
+                        guestList.set(j-1, guestList.get(j));
+                        guestList.set(j, temp);
+                    }
+                }
+                if (stop == true){
+                    break;
+                }
+//            for (int k = 0; k < nPeopleList.size(); k++){
+//                System.out.print(nPeopleList.get(k) + " ");
+//            }
+//            System.out.println();
+            }
+    }
+
+    public void sortByTimeMade(){
+
+        Reservation temp;
+        boolean stop = true;
+        for (int i = 0; i < guestList.size(); i++){
+            for (int j = 0; j < guestList.size(); j++) {
+                while (j != 0 && guestList.get(j).getTimeMade() < guestList.get(j-1).getTimeMade()){
+                    stop = false;
+                    temp = guestList.get(j-1);
+                    guestList.set(j-1, guestList.get(j));
+                    guestList.set(j, temp);
+                }
+            }
+            if (stop == true){
+                break;
+            }
+//            for (int k = 0; k < nPeopleList.size(); k++){
+//                System.out.print(nPeopleList.get(k) + " ");
+//            }
+//            System.out.println();
+        }
+    }
+
+    public static class RestaurantGUI implements ActionListener {
+        private JFrame mainFrame;
+        private JPanel mainPanel;
+        private JPanel northPanel;
+        private JPanel southPanel;
+        private JButton buttonPeople;
+        private JButton buttonTimeFor;
+        private JButton buttonTimeMade;
+        private JButton buttonAdd;
+        private JTextArea reservationList;
+
+        private JTextField inputText;
+
+        public RestaurantGUI() {
+
+            mainFrame = new JFrame();
+            mainPanel = new JPanel();
+            northPanel = new JPanel();
+            buttonPeople = new JButton("Sort by People");
+            buttonTimeFor = new JButton("Sort by Time For");
+            buttonTimeMade = new JButton("Sort by Time Made");
+            buttonAdd = new JButton("+");
+            reservationList = new JTextArea();
+            inputText = new JTextField();
+
+            mainFrame.setSize(1800, 900);
+            int borderSize = 50;
+
+            mainFrame.add(mainPanel);
+            mainPanel.add(northPanel, BorderLayout.NORTH);
+            mainPanel.add(southPanel, BorderLayout.SOUTH);
+
+            northPanel.setLayout(new GridLayout(1,3));
+            northPanel.add(buttonPeople);
+            northPanel.add(buttonTimeFor);
+            northPanel.add(buttonTimeMade);
+
+            mainPanel.add(reservationList);
+
+            southPanel.setLayout(new FlowLayout());
+            southPanel.add(buttonAdd);
+            southPanel.add(inputText);
+
+            mainFrame.setVisible(true);
+
+        }
+
+        public void convertToString(ArrayList<Reservation> guestList){
+            for (int i = 0; i < guestList.size(); i++){
+                guestList.get(i).printInfo();
+            }
+        }
+
+        public void actionPerformed(ActionEvent e){
+
+            Object buttonClicked = e.getSource();
+
+            if (buttonClicked == buttonPeople) {
+                System.out.println("Sort by People");
+                reservationList.setText(reservationList.getText());
+            }
+
+            if (buttonClicked == buttonTimeFor) {
+                System.out.println("Sort by Time For");
+                reservationList.setText(reservationList.getText());
+            }
+
+            if (buttonClicked == buttonTimeMade) {
+                System.out.println("Sort by Time Made");
+                reservationList.setText(reservationList.getText());
+            }
+
+            if (buttonClicked == buttonAdd) {
+//                reservationList.add();
+                System.out.println("button clicked");
+                String nPeople = inputText.getText();
+                System.out.println("new reservation for " + Integer.parseInt(nPeople) + " people");
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+//        int nOfReservations = 25;
+//        int nPeople;
+
+        Restaurant Mooo = new Restaurant();
+        RestaurantGUI GUI = new RestaurantGUI();
+    }
+}
+
+
+
+
